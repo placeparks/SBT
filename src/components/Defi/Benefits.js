7c92397427473a14e8c06591de4279fda1242919
placeparks <86215469+placeparks@users.noreply.github.com>
2023-05-21 13:55:21 -0700

    Update Benefits.js

--- src/components/Defi/Benefits.js
+++ src/components/Defi/Benefits.js
@@ -98,11 +98,6 @@ export default function Benefits(){
   </div>
   
 </div>
-<div className="d-grid gap-2 d-md-block">
-<button>CONNECT ON WHATSAPP</button>
-    <button>CONNECT ON TELEGRAM</button>
-    </div>
-
         </div>
       
     )
